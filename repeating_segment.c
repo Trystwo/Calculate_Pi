246f5ab95aab1a572e1da46d7eb13067ea8fd57e
Li Tianli <1031274793@qq.com>
2023-10-24 02:56:39 +0800

    计算n/m的循环节

--- repeating_segment.c
+++ repeating_segment.c
@@ -1,44 +1,33 @@
 /*计算1/n后10位的二进制小数*/
 #include "include.h"
-int repeating_segment(int input)
+int repeating_segment()
 {   int n = 1;
     int i;
-    int s;
-    while(n <= input)
+    int s = 0;
+    int m;
+    int t;
+    while(1)
     {
-        s = 1;
+        s = 0;
         i = 0;
-        //printf("请输入n的值：");
-        //scanf("%d",&n);//输入n
-        printf("1/%d的二进制小数为：",n);
+        
+        printf("请输入n,m的值：");
+        scanf("%d %d", &n, &m);//输入n
+        printf("n/m的循环节为：" );
+        s = n%m;
+        t = n;
+        printf("%d.",n/m);
         for(i=0;i<100;i++)
         {   
-            if(s>=n)
+            s = (n = s*10)%m;
+            printf("%d",n/m);
+            if(s == t)
             {
-                printf("1");
-                if(n==1)
-                {
-                    printf(".");
-                }  
-                s -= n;
-                if(s==1)
-                {
-                    printf(" 循环节长度:%d", i);
-                    break;
-                }
+                printf("\nn/m的循环节长度为：%d\n",i+1);
+                break;
             }
-            else
-            {
-                printf("0");
-                if(i==0)
-                {
-                    printf(".");
-                }
-            }
-            s = s*2;
         }
-        printf("\n");
-        n += 2;
+    
     }
     return 0;
 }
