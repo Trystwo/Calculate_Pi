63e7bcf4220328cc03aebeef7cdd755f94fb05c5
Li Tianli <1031274793@qq.com>
2023-10-25 01:22:17 +0800

    优化

--- repeating_segment.c
+++ repeating_segment.c
@@ -1,22 +1,19 @@
-/*计算1/n后10位的二进制小数*/
 #include "include.h"
+
+/*计算1/n后10位的二进制小数*/
 int repeating_segment()
-{   int n = 1;
-    long i;
-    int s = 0;
-    int m;
-    int t;
+{   
+    int n, i, s, m, t;
+
     while(1)
     {
         s = 0;
-        i = 0;
-        t = 0;
         printf("请输入n,m的值：");
         scanf("%d %d", &n, &m);//输入n,m
         printf("n/m的循环节为：" );
         s = n%m;
         printf("%d.",n/m);
-        while(n/m == 0)
+        while(n/m==0)
         {   
             t = n;
             s = (n = s*10)%m;
@@ -25,8 +22,14 @@ int repeating_segment()
         for(i=0;s!=t;i++)
         {    
             s = (n = s*10)%m;
-            printf("%d",n/m);
-            i++;
+
+             printf("%d",n/m);
+            //如果n/m不是循环小数，那么s==t，退出循环
+            if((s==0)||(s==t/10))
+            {
+                printf("\n");
+                break;
+            }
         }
         printf("\nn/m的循环节长度为：%d\n",i+1);
     
