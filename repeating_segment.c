b032b5ec883d8f347ad9acc7504cd272680cc799
bianxudong ys5115 (CW <YS.bianxudong@h3c.com>
2023-10-26 10:49:06 +0800

    优化循环节长度计算

--- repeating_segment.c
+++ repeating_segment.c
@@ -1,37 +1,38 @@
 #include "include.h"
 
+#define NON_ZERO !0
 /*计算1/n后10位的二进制小数*/
 int repeating_segment()
 {   
     int n, i, s, m, t;
-
+    unsigned char *a;
     while(1)
     {
-        s = 0;
         printf("请输入n,m的值：");
         scanf("%d %d", &n, &m);//输入n,m
-        printf("n/m的循环节为：" );
-        s = n%m;
-        printf("%d.",n/m);
-        while(n/m==0)
+        a  = calloc(m, sizeof(unsigned char));
+        printf("n/m的循环节为：%d.", n/m );
+        n = n%m;
+        for(i=0;a[n]==0;i++)
         {   
-            t = n;
-            s = (n = s*10)%m;
+            a[n] = i;
+            n = n*10;
             printf("%d",n/m);
+            n = n%m;
         }
-        for(i=0;s!=t;i++)
-        {    
-            s = (n = s*10)%m;
-
-             printf("%d",n/m);
-            //如果n/m不是循环小数，那么s==t，退出循环
-            if((s==0)||(s==t/10))
-            {
-                printf("\n");
-                break;
-            }
-        }
-        printf("\nn/m的循环节长度为：%d\n",i+1);
+        // for(i=0;n!=t;i++)
+        // {    
+        //     n*10;
+        //     printf("%d",n/m);
+        //     //如果n/m不是循环小数
+        //     n = n%m;
+        //     if((n==0)||(n==t/10))
+        //     {
+        //         printf("\n");
+        //         break;
+        //     }
+        // }
+        printf("\nn/m的循环节长度为：%d\n",i-a[n]);
     
     }
     return 0;
