eb2333a8f7bb77a8931c832a09412b4fb34cfbc1
Li Tianli <1031274793@qq.com>
2023-10-26 22:09:55 +0800

    123

--- repeating_segment.c
+++ repeating_segment.c
@@ -2,37 +2,36 @@
 
 /*计算1/n后10位的二进制小数*/
 int repeating_segment()
-{   
-    int n, i, s, m, t;
-
-    while(1)
+{
+    int i, n, m;
+    int s[100000];
+    while (1)
     {
-        s = 0;
+        i = 100;
         printf("请输入n,m的值：");
-        scanf("%d %d", &n, &m);//输入n,m
-        printf("n/m的循环节为：" );
-        s = n%m;
-        printf("%d.",n/m);
-        while(n/m==0)
-        {   
-            t = n;
-            s = (n = s*10)%m;
-            printf("%d",n/m);
-        }
-        for(i=0;s!=t;i++)
-        {    
-            s = (n = s*10)%m;
+        scanf("%d %d", &n, &m); // 输入n,m
+        printf("n/m的循环节为：%d.", n / m);
+        n = n % m;
 
-             printf("%d",n/m);
-            //如果n/m不是循环小数，那么s==t，退出循环
-            if((s==0)||(s==t/10))
-            {
-                printf("\n");
-                break;
-            }
+        while (i--)
+        {
+            n = n * 10;
+            printf("%d", n / m);
+            n = n % m;
         }
-        printf("\nn/m的循环节长度为：%d\n",i+1);
-    
+        // for (i = 0; s != t; i++)
+        // {
+        //     s = (n = s * 10) % m;
+
+        //     printf("%d", n / m);
+        //     // 如果n/m不是循环小数，退出循环
+        //     if ((s == 0) || (s == t / 10))
+        //     {
+        //         printf("\n");
+        //         break;
+        //     }
+        // }
+        printf("\nn/m的循环节长度为：%d\n", i + 1);
     }
     return 0;
 }
